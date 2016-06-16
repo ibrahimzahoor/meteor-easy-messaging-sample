2b4626f73a17514fcd25806e81342df0edab8ee6
Ibrahim Zahoor <ibrahimzahoor.nu@gmail.com>
2016-06-17 04:53:51 +0500

    package initialized

--- server/main.js
+++ server/main.js
@@ -1,1 +1,14 @@
 import '/imports/startup/server';
+
+
+// Import and rename a variable exported by easy-messaging.js.
+import { EasyMessaging } from "meteor/ibrahimzahoor:easy-messaging";
+// import { abc, abcd } from "meteor/ibrahimzahoor:easy-messaging";
+//
+// console.log("abc", packageName.abc);
+// console.log("abcd", packageName.abcd);
+//
+console.log("packageName", EasyMessaging);
+// // console.log("packageName", name);
+//
+console.log(" server ", Package["ibrahimzahoor:easy-messaging"]);
